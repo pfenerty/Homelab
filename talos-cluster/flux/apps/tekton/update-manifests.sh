c7f66c3a62ca7c5301f8e54dacedd1777a22ba14
Patrick Fenerty <patrick@fenerty.me>
2026-03-15 15:32:00 -0500

    add tekton interceptors

--- talos-cluster/flux/apps/tekton/update-manifests.sh
+++ talos-cluster/flux/apps/tekton/update-manifests.sh
@@ -4,3 +4,4 @@
 curl -Lo pipelines.yaml https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 curl -Lo triggers.yaml https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 curl -Lo dashboard.yaml https://storage.googleapis.com/tekton-releases/dashboard/latest/release.yaml
+curl -Lo interceptors.yaml https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
